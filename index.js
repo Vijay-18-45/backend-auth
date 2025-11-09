e261fd0dd72433e94ab13f13d05999e6f8cbedfa
vijay-18-45 <vijayjosephchinni367@gmail.com>
2025-11-09 11:57:14 +0530

    Final CORS fix: Added live Vercel domain

--- index.js
+++ index.js
@@ -14,8 +14,16 @@ app.use(express.urlencoded({extended:true}))
  
 
 
+
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://127.0.0.1:3000", 
+  "https://autho-authen.vercel.app" 
+];
+
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: allowedOrigins,
   credentials: true,
 }));
 
