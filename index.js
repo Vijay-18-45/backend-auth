e9e6fa4f4d00311ebc1f85a6ca5ab0e3a79a146a
vijay-18-45 <vijayjosephchinni367@gmail.com>
2025-11-20 12:24:43 +0530

    second redeploy

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const helmet = require('helmet')
-const mongoose = require('mongoose')
-const authRouter = require('./routers/authRouter')
-app.use(helmet())
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const authRouter = require('./routers/authRouter');
 
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
- 
-
+// --- FIX START ---
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "https://autho-authen.vercel.app"
 ];
 
-app.use(cors({
+const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
@@ -28,33 +27,25 @@ app.use(cors({
     }
   },
   credentials: true
-}));
+};
 
 
+app.options("*", cors(corsOptions));
+
+app.use(cors(corsOptions));
 
 
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
 
+app.use('/api/auth', authRouter);
 
+app.get('/', (req, res) => {
+  res.json({ message: "Hello from the server" });
+});
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log("Database connected")
-}).catch((err)=>{
-    console.log(err)
+app.listen(process.env.PORT, () => {
+  console.log("listening on port", process.env.PORT);
+});
 
-})
-
-
-
-
-app.use('/api/auth',authRouter)
-
-app.get('/',(req,res)=>{
-
-     res.json({message:"Hello from the server"})
-
-})
-app.listen(process.env.PORT,()=>{
-
-     console.log("listening on port 8000")
-
-})
